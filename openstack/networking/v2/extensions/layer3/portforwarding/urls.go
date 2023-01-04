15f8c29e9e6701bf246223f6830f253b18b7579c
Sean Oh <innodreamer@gmail.com>
2023-01-04 21:14:22 +0900

    Add ExtractJobInfo() method for PortForwarding Job

--- openstack/networking/v2/extensions/layer3/portforwarding/urls.go
+++ openstack/networking/v2/extensions/layer3/portforwarding/urls.go
@@ -1,13 +1,19 @@
 package portforwarding
 
-import "github.com/cloud-barista/ktcloudvpc-sdk-for-drv"
+import (
+	"strings"
+	"github.com/cloud-barista/ktcloudvpc-sdk-for-drv"
+)
 
-const resourcePath = "floatingips"
+const resourcePath = "Portforwarding"
 const portForwardingPath = "port_forwardings"
 
-func portForwardingUrl(c *gophercloud.ServiceClient, id string) string {
-	return c.ServiceURL(resourcePath, id, portForwardingPath)
+func portForwardingUrl(c *gophercloud.ServiceClient) string {
+	return strings.Replace(c.ServiceURL(resourcePath, "", ""), "Portforwarding//", "Portforwarding", 1) 
 }
+// func portForwardingUrl(c *gophercloud.ServiceClient, id string) string {
+// 	return c.ServiceURL(resourcePath, id, portForwardingPath)
+// }
 
 func singlePortForwardingUrl(c *gophercloud.ServiceClient, id string, portForwardingID string) string {
 	return c.ServiceURL(resourcePath, id, portForwardingPath, portForwardingID)
